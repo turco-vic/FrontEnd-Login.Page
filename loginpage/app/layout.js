b0dc218e27aeeac8a78a52327be8eb9eb1a04295
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-03-06 12:11:30 -0300

    Update layout.js

--- loginpage/app/layout.js
+++ loginpage/app/layout.js
@@ -1,11 +1,12 @@
 import "../styles/globals.css";
-			export const metadata = {
-				title: "My First NextJS App",
-			};
-			export default function RootLayout({ children }) {
-				return (
-					<html>
-						<body>{children}</body>
-					</html>
-);
-		}
+
+export const metadata = {
+  title: "My First NextJS App",
+};
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
