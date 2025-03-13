458cc330bea0eaf1e80381b9c6adc49702f3dfbf
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-03-12 22:33:26 -0300

    Update Styles

--- app/page.jsx
+++ app/page.jsx
@@ -1,29 +1,26 @@
 import styles from "../styles/login.module.css"
 import Input from "../components/input"
 
-export default function login(){
-return (
-    <div className={styles.container}>   
-    <div className={styles.form}>
-    <div className={styles.text}>
-        <h1>Login</h1>
-        <h6>Bem vindo ao Login</h6>
-    </div>
-    <div className={styles.inputs}>
-        <Input text = "Username"/>
-        <Input text = "Password"/>
-    <div className={styles.button}>
-        <button>Login</button>
-    </div>
-    <div className={styles.button}>
-        <button>Cadastro</button>
-    </div>
-    </div>
-        <div className={styles.b1}></div>
-        <div className={styles.b2}></div>
-        <div className={styles.b3}></div>
-        <div className={styles.b4}></div>
-    </div>
+export default function login() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.form}>
+        <div className={styles.text}>
+          <h1>Login</h1>
+        </div>
+        <div className={styles.inputstxt}>
+          <Input text="Username" />
+          <Input text="Password" />
+        </div>
+        <div className={styles.check}>
+          <div className={styles.buttons}>
+            <button>Login</button>
+          </div>
+          <div className={styles.buttons}>
+            <button>Cadastro</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
