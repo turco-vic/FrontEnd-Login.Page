9ca018285a1afd9163de2cfce138db7c4928bb72
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-03-12 21:52:18 -0300

    Create input.jsx

---
+++ components/input.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/input.module.css"
+
+export default function Input({text}){
+    return (
+        <div>
+            <input className={styles.input} type="text" placeholder={text}/>
+        </div>
+    )
+}
