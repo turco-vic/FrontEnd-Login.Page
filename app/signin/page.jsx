d60858940849853c7cab280ddaa6003efbdea943
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-03-20 11:46:27 -0300

    Update Props Img

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -3,6 +3,14 @@ import styles from '../../styles/Signin.module.css';
 import Card from '../../components/Card';
 
 export default function Initial() {
+    const tecnologiasImgs = [
+        "https://static-00.iconduck.com/assets.00/nextjs-icon-2048x2048-eugu5rfi.png",
+        "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+        "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png"
+    ];
+
     return (
         <div className={styles.container}>
             <div className={styles.return}>
@@ -19,8 +27,8 @@ export default function Initial() {
                 </div>
                 <Card title={"Sobre mim"} text={"Olá! Meu nome é Enzo, tenho 17 anos, e atualmente estou cursando Desenvolvimento de sistema no SENAI Valinhos!"} />
                 <Card title={"Objetivo"} text={"Este site foi feito com o propósito de realizar a ativade de Front-End utilizando Next.JS!"} />
-                <Card title={"Tecnologias que utilizo:"} img={"https://static-00.iconduck.com/assets.00/nextjs-icon-2048x2048-eugu5rfi.png"} img />
+                <Card title={"Tecnologias que utilizo:"} imgs={tecnologiasImgs} />
             </div>
         </div>
-    )
+    );
 }
--- components/card.jsx
+++ components/card.jsx
@@ -1,11 +1,17 @@
-import styles from "../styles/Card.module.css"
+import styles from "../styles/Card.module.css";
 
-export default function Card({ title, text, img }) {
+export default function Card({ title, text, imgs }) {
     return (
         <div className={styles.card}>
             <h1>{title}</h1>
-            <p>{text}</p>
-            <img>{img}</img>
+            {text && <p>{text}</p>}
+            {imgs && (
+                <div className={styles.images}>
+                    {imgs.map((img, index) => (
+                        <img key={index} src={img} alt={`Imagem ${index + 1}`} className={styles.image} />
+                    ))}
+                </div>
+            )}
         </div>
-    )
+    );
 }
